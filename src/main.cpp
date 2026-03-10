d92ea0b20076e4fd8961c123e0d8165d0099b409
DuckyLine <legofftimeo2000@gmail.com>
2026-03-10 15:01:06 +0100

    add formation of cube

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "../includes/Parsing.hpp"
+#include "../includes/Cube.hpp"
 
 int main(int argc, char **argv) {
 
@@ -9,6 +10,8 @@ int main(int argc, char **argv) {
 		for (size_t i = 0; i < actions.size(); i++) {
 			std::cout << "Step " << i << " = " << actions[i] << std::endl;
 		}
+
+		Cube cube({'Y', 'B', 'R', 'G', 'W', 'O'}, 9);
 
 	} catch (const std::runtime_error& e) {
 		std::cout << "Error : " << e.what() << std::endl;
